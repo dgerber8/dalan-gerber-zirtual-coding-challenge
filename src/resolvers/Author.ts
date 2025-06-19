e4baa2b34e1f3aaa1227e79d1ea164570fb5dea5
Dalan Gerber <8gerberd@gmail.com>
2025-06-19 14:32:38 -0600

    Added displayName handling for countries where the last name comes first

--- src/resolvers/Author.ts
+++ src/resolvers/Author.ts
@@ -1,16 +1,23 @@
 import { Context } from "../types";
 import Countries from "../lib/Countries"
 
+const FAMILY_FIRST_COUNTRIES = ["JP"]
+
 const countriesClient = new Countries();
 
 const Author = {
   displayName: (author: any) => {
-    // author will have givenName and familyName from DB
+    // If the country is in FAMILY_FIRST_COUNTRIES, we display family name first
+    if (FAMILY_FIRST_COUNTRIES.includes(author.countryCode)) {
+      return `${author.familyName} ${author.givenName}`;
+    }
+    // Otherwise, we display given name first
     return `${author.givenName} ${author.familyName}`;
   },
   country: async (author: any) => {
     const country = await countriesClient.searchByCode(author.countryCode);
     
+    // If the country is not found, we return null
     return country?.name.common ?? null;
   }
 }
